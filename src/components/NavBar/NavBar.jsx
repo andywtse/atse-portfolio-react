1a54468e2ea57846ef8cc0e59122b29971264ecd
andywtse <atse4995@email.com>
2022-08-30 16:11:32 -0400

    Modifies navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,16 @@
 import { Link } from 'react-router-dom'
 
-const NavBar = ({ user, handleLogout }) => {
+const NavBar = props => {
   return (
     <>
-      {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
-        </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
+      <nav>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/work">Work</Link></li>
+          <li><Link to="/resume">Resume</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
     </>
   )
 }
